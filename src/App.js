a8eda3243e238b915268a4405b8f92c4220eb4f2
Aaron Soto <aaron.m.soto1@gmail.com>
2021-07-17 08:59:22 -0700

    updated google analytics setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from "react-router-dom";
 import { BlogList } from "./components/ui/blog-list/BlogList";
+import { BrowserRouter } from "react-router-dom";
 import { Footer } from "./components/ui/footer/Footer";
 import { Navbar } from "./components/ui/navbar/Navbar";
 import { SinglePost } from "./components/ui/single-post/SinglePost";
@@ -7,14 +8,22 @@ import { SinglePost } from "./components/ui/single-post/SinglePost";
 // import { WorksList } from "./components/ui/works-list/WorksList";
 import { ComingSoon } from "./components/ui/coming-soon/ComingSoon";
 import RouteChangeTracker from "./data/RouteChangeTracker";
+import { createBrowserHistory } from "history";
 import ReactGA from "react-ga";
+
+const history = createBrowserHistory();
 
 ReactGA.initialize("UA-177845106-1");
 ReactGA.pageview(window.location.pathname);
 
+history.listen((location, action) => {
+	ReactGA.set({ page: location.pathname });
+	ReactGA.pageview(location.pathname);
+});
+
 function App() {
 	return (
-		<>
+		<BrowserRouter history={history}>
 			<Navbar />
 			<RouteChangeTracker />
 			<Switch>
@@ -27,7 +36,7 @@ function App() {
 				<Route exact path='/' component={BlogList} />
 			</Switch>
 			<Footer />
-		</>
+		</BrowserRouter>
 	);
 }
 
