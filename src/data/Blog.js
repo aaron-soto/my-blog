7c7e94b77060adf276a2d6eef87f088d31eaea63
Aaron Soto <aaron.m.soto1@gmail.com>
2021-08-08 13:12:56 -0700

    added next blog-post

--- src/data/Blog.js
+++ src/data/Blog.js
@@ -8,4 +8,13 @@ export const blogs = [
 		description:
 			"Everyone has their own boilerplate that they like to work with when creating a react app. I show you how I go about starting any react app with the inital setup.",
 	},
+	{
+		title: "Why You Should Go To A Coding Bootcamp",
+		slug: "why-you-should-go-to-a-coding-bootcamp",
+		date: "August 08, 2021",
+		categories: ["Career", "Bootcamp", "Personal Growth"],
+		path: `2021/2021-08-08-why-you-should-go-to-a-coding-bootcamp/why-you-should-go-to-a-coding-bootcamp.md`,
+		description:
+			"I get asked alot if attending the coding bootcamp that I did was worth it, I would like to tell you why I enjoyed it and how I benefited from it and how you will too.",
+	},
 ];
